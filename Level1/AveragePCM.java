968968e3e320e0354b1624e8c6e95e5e8eb1e31c
Rishabh2155 <123048177+Rishabh2155@users.noreply.github.com>
2025-08-22 12:35:47 +0530

    Delete Level1 directory

--- Level1/AveragePCM.java
+++
@@ -1,9 +0,0 @@
-class AveragePCM {
-    public static void main(String[] args) {
-        int maths = 94, physics = 95, chemistry = 96;
-        int average = (maths + physics + chemistry) / 3;
-
-        System.out.println("average mark in PCM is " + average);
-    }
-}
-
