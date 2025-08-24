7bbb3abe4d94c1a8e6690b8491aa1245f61e4dcb
Rishabh2155 <singhhappy89711@gmail.com>
2025-08-24 11:45:23 +0530

    Level-2

---
+++ Level-2/YoungestTallestFriend.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class YoungestTallestFriend {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Amar's age: ");
+        int ageAmar = sc.nextInt();
+        System.out.print("Enter Akbar's age: ");
+        int ageAkbar = sc.nextInt();
+        System.out.print("Enter Anthony's age: ");
+        int ageAnthony = sc.nextInt();
+        System.out.print("Enter Amar's height: ");
+        int heightAmar = sc.nextInt();
+        System.out.print("Enter Akbar's height: ");
+        int heightAkbar = sc.nextInt();
+        System.out.print("Enter Anthony's height: ");
+        int heightAnthony = sc.nextInt();
+        String youngest;
+        if (ageAmar < ageAkbar && ageAmar < ageAnthony) {
+            youngest = "Amar";
+        } else if (ageAkbar < ageAnthony) {
+            youngest = "Akbar";
+        } else {
+            youngest = "Anthony";
+        }
+        String tallest;
+        if (heightAmar > heightAkbar && heightAmar > heightAnthony) {
+            tallest = "Amar";
+        } else if (heightAkbar > heightAnthony) {
+            tallest = "Akbar";
+        } else {
+            tallest = "Anthony";
+        }
+        System.out.println("The youngest friend is: " + youngest);
+        System.out.println("The tallest friend is: " + tallest);
+    }
+}
